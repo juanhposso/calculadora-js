997a285d29b7ea0098f4e06e4c0db3ab871b0b24
juanhposso <juan.hernandez0793@gmail.com>
2021-08-24 22:57:10 -0500

    calculadora terminada falta realizar operaciones con varios operadores

--- main.js
+++ main.js
@@ -1,16 +1,96 @@
 const $inputElement = document.querySelector('input');
-const $buttonsElement = Array.from(document.querySelectorAll('button'));
+const $buttonsElement = Array.from(document.querySelectorAll('.numero'));
+const $buttonSum = document.querySelector('.mas');
+const $buttonRes = document.querySelector('.menos');
+const $buttonPor = document.querySelector('.por');
+const $buttonSlash = document.querySelector('.slash');
+const $buttonReset = document.querySelector('.reset');
+
+const $buttonIgual = document.querySelector('.igual');
 
 $buttonsElement.forEach((button) => {
-	button.addEventListener('click', (event) => {
-		let input = $inputElement.value;
-		if (input == '0') {
-			console.log($inputElement, ' if jijij');
-			return $inputElement.setAttribute('value', event.target.value);
+	button.onclick = (event) => {
+		if (parseInt($inputElement.value) === 0) {
+			$inputElement.setAttribute('value', event.target.value);
 		} else {
-			let resultado = input.concat(event.target.value);
-			console.log(resultado);
-			return $inputElement.setAttribute('value', resultado);
+			let test = $inputElement.value.concat(event.target.value);
+			$inputElement.setAttribute('value', test);
 		}
-	});
+	};
 });
+
+// Sumar
+$buttonSum.onclick = () => {
+	$inputElement.setAttribute('value', $inputElement.value + ' + ');
+};
+
+// restar
+$buttonRes.onclick = () => {
+	$inputElement.setAttribute('value', $inputElement.value + ' - ');
+};
+
+// Multiplicar
+$buttonPor.onclick = () => {
+	$inputElement.setAttribute('value', $inputElement.value + ' x ');
+};
+
+// Dividir
+$buttonSlash.onclick = () => {
+	$inputElement.setAttribute('value', $inputElement.value + ' / ');
+};
+
+// Reset
+$buttonReset.onclick = () => {
+	$inputElement.setAttribute('value', '0');
+};
+
+// Imprimir resultado
+$buttonIgual.onclick = () => {
+	const sumCheck = $inputElement.value.includes('+');
+	const resCheck = $inputElement.value.includes('-');
+	const byCheck = $inputElement.value.includes('x');
+	const divCheck = $inputElement.value.includes('/');
+
+	if (sumCheck) {
+		const sumarString = $inputElement.value.split('+');
+		const sumar = sumarString.map((sum) => {
+			return parseInt(sum);
+		});
+
+		const resultado = sumar.reduce((total, current) => {
+			return total + current;
+		});
+		//console.log(toString(resultado));
+		$inputElement.setAttribute('value', resultado.toString());
+	} else if (resCheck) {
+		const restarString = $inputElement.value.split('-');
+		const restar = restarString.map((res) => {
+			return parseInt(res);
+		});
+
+		const resultado = restar.reduce((total, current) => {
+			return total - current;
+		});
+		$inputElement.setAttribute('value', resultado.toString());
+	} else if (byCheck) {
+		const multiplicarString = $inputElement.value.split('x');
+		const multiplicar = multiplicarString.map((res) => {
+			return parseInt(res);
+		});
+
+		const resultado = multiplicar.reduce((total, current) => {
+			return total * current;
+		});
+		$inputElement.setAttribute('value', resultado.toString());
+	} else if (divCheck) {
+		const dividirString = $inputElement.value.split('/');
+		const dividir = dividirString.map((res) => {
+			return parseInt(res);
+		});
+
+		const resultado = dividir.reduce((total, current) => {
+			return total / current;
+		});
+		$inputElement.setAttribute('value', resultado.toString());
+	}
+};
